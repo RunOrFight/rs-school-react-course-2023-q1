008f1b3f2e5959b8567ca174c837abe2b2420c4d
RunOrFight <dragoinfury@gmail.com>
2023-03-22 16:42:52 +0300

    fix: translate the functional component CardList to class component

--- src/components/CardList/index.tsx
+++ src/components/CardList/index.tsx
@@ -1,24 +1,32 @@
 import { Card } from 'components';
-import React, { useEffect, useState } from 'react';
+import React, { Component } from 'react';
 import { IProduct } from 'types';
 import css from './style.module.css';
 
-const CardList = () => {
-  const [products, setProducts] = useState<IProduct[]>([]);
-
-  useEffect(() => {
+interface CardListState {
+  products: IProduct[];
+}
+class CardList extends Component<object, CardListState> {
+  constructor(props: object) {
+    super(props);
+    this.state = {
+      products: [],
+    };
+  }
+  componentDidMount() {
     fetch('https://dummyjson.com/products')
       .then((res) => res.json())
-      .then((json) => setProducts(json.products));
-  }, []);
-
-  return (
-    <div className={css.container}>
-      {products.map((product) => (
-        <Card key={product.id} product={product}></Card>
-      ))}
-    </div>
-  );
-};
+      .then((json) => this.setState({ products: json.products }));
+  }
+  render(): React.ReactNode {
+    return (
+      <div className={css.container}>
+        {this.state.products.map((product) => (
+          <Card key={product.id} product={product}></Card>
+        ))}
+      </div>
+    );
+  }
+}
 
 export default CardList;
